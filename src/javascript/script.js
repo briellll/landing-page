02e1cced3c124a415c533c032f5c4b7cdeefa371
Gabriel <carloscgmorais@gmail.com>
2026-03-11 15:27:35 -0300

    Adiciona animações de scroll e ativações de seção no menu de navegação

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -23,22 +23,71 @@ function scrollAnimation(){
 
     window.addEventListener('scroll', function (){
 
+        //animação de sombra no header
         const scrollPosition = window.scrollY - header.offsetHeight;
-
         if (scrollPosition <= 0){
             header.classList.remove('header-move');
         } else {
             header.classList.add('header-move')
         }
 
+        //animação de cor no bottom dos botoes da nav
+        const scroll = window.scrollY;
+        let activeSectionIndex = 0;
+
+        sections.forEach((section,i) =>{
+            const sectionTop = section.offsetTop - 96;
+            const sectionBottom = sectionTop + section.offsetHeight;
+
+            if (scroll >= sectionTop && scroll < sectionBottom){
+                activeSectionIndex = i;
+            }
+        });
+
+        navItems.forEach(item => item.classList.remove('active'))
+
+        if(navItems[activeSectionIndex]){
+            navItems[activeSectionIndex].classList.add('active');
+        }
+
+        //animacao ao scrollar a pagina
+
+
     })
 }
 
+function animatePage(){
 
+    ScrollReveal().reveal('#cta', {
+            origin: 'left',
+            duration: 2000,
+            distance: '20%'
+        });
+
+        ScrollReveal().reveal('.dish', {
+            origin: 'left',
+            duration: 2000,
+            distance: '20%'
+        });
+
+        ScrollReveal().reveal('#testimonial_chef', {
+            origin: 'left',
+            duration: 1000,
+            distance: '20%'
+        });
+
+        ScrollReveal().reveal('.feedback', {
+            origin: 'right',
+            duration: 1000,
+            distance: '20%'
+        });
+
+}
 
 
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
     scrollAnimation();
+    animatePage();
 })
